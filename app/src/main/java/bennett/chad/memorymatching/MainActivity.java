1cf9e1811bef73ce8d90bcd4c09c7da49219ce9f
bennettcab <bennettcab@gmail.com>
2016-11-29 19:22:07 -0500

    - Added handling of menu item events (just displays toasts for now)

--- app/src/main/java/bennett/chad/memorymatching/MainActivity.java
+++ app/src/main/java/bennett/chad/memorymatching/MainActivity.java
@@ -3,6 +3,8 @@ package bennett.chad.memorymatching;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +18,19 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_memorymatching, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_reset:
+                Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.menu_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }

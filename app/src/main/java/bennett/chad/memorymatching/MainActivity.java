c873e8c26839a3783e2b7a7e6a1c705a154989ef
bennettcab <bennettcab@gmail.com>
2016-11-30 18:36:13 -0500

    Added Menu icons - Changed the menu file name

--- app/src/main/java/bennett/chad/memorymatching/MainActivity.java
+++ app/src/main/java/bennett/chad/memorymatching/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_memorymatching, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
